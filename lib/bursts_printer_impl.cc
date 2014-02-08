6c692872b651a1e803ef006cb3c9adb2b49755c5
piotr <Piotr Krysik pkrysik@elka.pw.edu.pl>
2014-02-08 14:16:26 +0100

    Added simple bursts printer

---
+++ lib/bursts_printer_impl.cc
@@ -0,0 +1,72 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2014 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "bursts_printer_impl.h"
+
+namespace gr {
+  namespace gsm {
+    void bursts_printer_impl::bursts_print(pmt::pmt_t burst)
+    {
+        int ii;
+        size_t burst_len;
+        int8_t *burst_elements = pmt::s8vector_writable_elements(burst, burst_len);
+
+        for(ii=0; ii<burst_len; ii++)
+        {
+           std::cout << std::setprecision(1) << static_cast<int>(burst_elements[ii]) << "";
+        }
+        std::cout << std::endl;
+    }
+
+    bursts_printer::sptr
+    bursts_printer::make()
+    {
+      return gnuradio::get_initial_sptr
+        (new bursts_printer_impl());
+    }
+
+    /*
+     * The private constructor
+     */
+    bursts_printer_impl::bursts_printer_impl()
+      : gr::block("bursts_printer",
+              gr::io_signature::make(0, 0, 0),
+              gr::io_signature::make(0, 0, 0))
+    {
+        message_port_register_in(pmt::mp("bursts"));
+        set_msg_handler(pmt::mp("bursts"), boost::bind(&bursts_printer_impl::bursts_print, this, _1));
+    }
+    
+    /*
+     * Our virtual destructor.
+     */
+    bursts_printer_impl::~bursts_printer_impl()
+    {
+    }
+
+
+  } /* namespace gsm */
+} /* namespace gr */
+
---
+++ lib/bursts_printer_impl.h
@@ -0,0 +1,43 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2014 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_GSM_BURSTS_PRINTER_IMPL_H
+#define INCLUDED_GSM_BURSTS_PRINTER_IMPL_H
+
+#include <gsm/bursts_printer.h>
+
+namespace gr {
+  namespace gsm {
+
+    class bursts_printer_impl : public bursts_printer
+    {
+     private:
+      void bursts_print(pmt::pmt_t burst);
+    
+     public:
+      bursts_printer_impl();
+      ~bursts_printer_impl();
+    };
+
+  } // namespace gsm
+} // namespace gr
+
+#endif /* INCLUDED_GSM_BURSTS_PRINTER_IMPL_H */
+
