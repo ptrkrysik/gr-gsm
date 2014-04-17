c1d47df7ba139c97b4511d7f14495ef39d9bf13a
piotr <Piotr Krysik pkrysik@elka.pw.edu.pl>
2014-04-17 09:45:50 +0200

    Added author

--- lib/receiver_impl.cc
+++ lib/receiver_impl.cc
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2014 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2014 Piotr Krysik <pkrysik@elka.pw.edu.pl>.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
