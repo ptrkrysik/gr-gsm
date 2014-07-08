6b78abc7bde9fafe1559208c16b282096af81a52
piotr <Piotr Krysik pkrysik@elka.pw.edu.pl>
2014-07-08 23:29:13 +0200

    Added new blocks written in python for new experimental gsm receiver. FCCH burst tagger is element of hierarchical block - FCCH detector that adds tags after every detected FCCH burst. The value of each tag is a frequency offset estimate. SCH detector waits for tags from FCCH detector which are used to find SCH burst position. It is unfinished.

--- python/__init__.py
+++ python/__init__.py
@@ -46,6 +46,10 @@ from gsm_swig import *
 
 # import any pure python here
 from receiver_hier import receiver_hier
+
+from fcch_burst_tagger import fcch_burst_tagger
+from sch_detector import sch_detector
+from fcch_detector import fcch_detector
 #
 
 # ----------------------------------------------------------------
