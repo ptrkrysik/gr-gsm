45e4c85f082f6e49b436a0bb4269ad3c3a4067a7
Ash Wilson <ash.d.wilson@gmail.com>
2016-01-30 16:12:21 -0800

    Hacky hacky hack

--- build_test_scripts/decode.sh
+++ build_test_scripts/decode.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export AP_DECODE=/usr/local/bin/airprobe_decode.py
+export CAPFILE=/test_data/vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile
 # We expect $CAPFILE to be defined.  If not, exit...
-if test -z $CAPFILE ; then
-  echo "Missing CAPFILE environment variable."
-  exit 1
-fi
+
 /usr/bin/python $AP_DECODE -c $CAPFILE
