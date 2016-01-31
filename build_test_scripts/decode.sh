d52d47e066c067c920b890692c03b369283482e6
Ash Wilson <ash.d.wilson@gmail.com>
2016-01-30 20:01:34 -0800

    Working on testing script

--- build_test_scripts/decode.sh
+++ build_test_scripts/decode.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export AP_DECODE=/usr/local/bin/airprobe_decode.py
-export CAPFILE=/src/test_data/vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile
-
+export AP_DECODE="/usr/local/bin/airprobe_decode.py"
+export CAPFILE="vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile"
+export TEST_FIXTURE_DIRECTORY="/src/test_data/"
 export RUNLINE="$AP_DECODE -c $CAPFILE -s $((100000000/174)) -a 725 -m BCCH -t 0"
-echo "Testing with:\n  $RUNLINE"
+echo "Testing with:"
+echo "  $RUNLINE"
+cd $TEST_FIXTURE_DIRECTORY
 $RUNLINE
