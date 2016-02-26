0884c2e5ae0e78ac2d51eb3f4de147a0df5a34b2
Ash Wilson <ash.d.wilson@gmail.com>
2016-02-25 23:07:50 -0600

    re-structuring testing stuff, adding testing docs and integration tests.

--- test/tests/decode.sh
+++ test/tests/decode.sh
@@ -2,7 +2,7 @@
 export AP_DECODE="grgsm_decode"
 export CAPFILE="vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile"
 export TEST_DATA_DIRECTORY="/src/test_data/"
-export TEST_CONTROL_DIRECTORY="/src/build_test/control"
+export TEST_FIXTURES_DIRECTORY="/src/test/fixtures"
 export RUNLINE="$AP_DECODE -c $CAPFILE -s $((100000000/174)) -a 725 -m BCCH -t 0 -v "
 echo "Testing with:"
 echo "  $RUNLINE"
