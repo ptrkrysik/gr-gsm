d169a249ce8addb3537f3ee6de821ecacbee98ca
Piotr Krysik <ptrkrysik@gmail.com>
2016-03-13 18:54:59 +0100

    Shortening decoding test and making it easier to run the test locally

--- build_test/scripts/decode.sh
+++ build_test/scripts/decode.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 export AP_DECODE="grgsm_decode"
-export CAPFILE="vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile"
-export TEST_DATA_DIRECTORY="/src/test_data/"
-export TEST_CONTROL_DIRECTORY="/src/build_test/control"
-export RUNLINE="$AP_DECODE -c $CAPFILE -s $((100000000/174)) -m BCCH -t 0 -v "
+export CAPFILE="../../test_data/vf_call6_a725_d174_g5_Kc1EF00BAB3BAC7002.cfile"
+export SHORTENED_CAPFILE="tmp.cfile"
+export RESULT_EXPECTED="../control/grgsm_decode_test1_expected"
+export RESULT_OBTAINED="grgsm_decode_test1_result"
+export RUNLINE="$AP_DECODE -c $SHORTENED_CAPFILE -s $((100000000/174)) -m BCCH -t 0 -v "
 echo "Testing with:"
 echo "  $RUNLINE"
-cd $TEST_DATA_DIRECTORY
 gnuradio-companion --version
 
-$RUNLINE | tail -n +4 | tee grgsm_decode_test1_result
-diff grgsm_decode_test1_result $TEST_CONTROL_DIRECTORY/grgsm_decode_test1_expected
+cat $CAPFILE | head -c 6000000 > $SHORTENED_CAPFILE
+
+$RUNLINE | tail -n +4 | tee $RESULT_OBTAINED
+diff $RESULT_EXPECTED $RESULT_OBTAINED
 TEST_RESULT=$?
 
-rm grgsm_decode_test1_result
+rm $RESULT_OBTAINED
+rm $SHORTENED_CAPFILE
+
 if [ $TEST_RESULT == 0 ]
 then
   echo "   Result: PASSED"
