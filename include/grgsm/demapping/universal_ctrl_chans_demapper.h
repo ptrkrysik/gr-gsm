bac8284599e137981f178e8998d6426f011d9dab
Piotr Krysik <ptrkrysik@gmail.com>
2016-05-15 10:39:39 +0200

    Added default values for uplink parameters so urrent Python code works without modification.

--- include/grgsm/demapping/universal_ctrl_chans_demapper.h
+++ include/grgsm/demapping/universal_ctrl_chans_demapper.h
@@ -49,7 +49,7 @@ namespace gr {
        * class. gsm::universal_ctrl_chans_demapper::make is the public interface for
        * creating new instances.
        */
-      static sptr make(unsigned int timeslot_nr, const std::vector<int> &downlink_starts_fn_mod51, const std::vector<int> &downlink_channel_types, const std::vector<int> &uplink_starts_fn_mod51, const std::vector<int> &uplink_channel_types);
+      static sptr make(unsigned int timeslot_nr, const std::vector<int> &downlink_starts_fn_mod51, const std::vector<int> &downlink_channel_types, const std::vector<int> &uplink_starts_fn_mod51=std::vector<int>(), const std::vector<int> &uplink_channel_types=std::vector<int>());
     };
 
   } // namespace gsm
