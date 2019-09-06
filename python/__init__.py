783a1f7b5625c152d6a7b535def897a7833bebec
Vasil Velichkov <vvvelichkov@gmail.com>
2019-09-07 02:46:30 +0300

    Remove the RTLD_GLOBAL hack
    It was removed from gnuradio back in 2014
    https://github.com/gnuradio/gnuradio/commit/39f14138ac9158eb0610e89a2ae1f8290f184c44
    
    Change-Id: I1604ad5b9eefe3d4a2228b2a09e2069962844c97

--- python/__init__.py
+++ python/__init__.py
@@ -23,23 +23,6 @@ This is the GNU Radio GSM module. Place your Python package
 description here (python/__init__.py).
 '''
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
-try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
-except ImportError:
-    try:
-        from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-        pass
-
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
 
 # import swig generated symbols into the gsm namespace
 from .grgsm_swig import *
@@ -59,12 +42,3 @@ from .fn_time import *
 from .txtime_bursts_tagger import *
 from .arfcn import *
 from .device import *
-
-
-#
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
